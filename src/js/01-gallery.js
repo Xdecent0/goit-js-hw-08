a65b6c30761f4701f54892be911f10cf2159a9f3
Xdecent <100188867+Xdecent0@users.noreply.github.com>
2022-08-04 13:02:49 +0300

    task 1/2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+const itemMarkup = galleryItems
+  .map(({ original, preview, description }) => {
+    return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    `;
+  })
+  .join('');
+gallery.insertAdjacentHTML('beforeend', itemMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
