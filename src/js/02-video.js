9e9204e79f05837f9de576399d27df8b83391593
Xdecent <100188867+Xdecent0@users.noreply.github.com>
2022-08-05 07:51:58 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,8 +2,10 @@ import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
+if (localStorage.getItem('videoplayer-current-time') === null) {
+  localStorage.setItem(`videoplayer-current-time`, 0);
+}
 const player = new Player(iframe);
-
 const onPlay = function ({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 };
