92509428b8472ed2c4278503b304830754b63493
Xdecent <100188867+Xdecent0@users.noreply.github.com>
2022-08-06 09:11:47 +0300

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,13 +3,15 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form input');
 const message = document.querySelector('.feedback-form textarea');
-const formData = {};
 GetData();
 form.addEventListener('input', throttle(onForm, 500));
 form.addEventListener('submit', onSubmit);
 
-function onForm(e) {
-  formData[e.target.name] = e.target.value;
+function onForm() {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 function onSubmit(e) {
@@ -19,14 +21,9 @@ function onSubmit(e) {
   localStorage.removeItem('feedback-form-state');
 }
 function GetData() {
-  if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
-    const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-    if (data.email) {
-      email.value = data.email;
-    }
-    if (data.message) {
-      message.value = data.message;
-    }
+  let data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (data !== null) {
+    email.value = data.email;
+    message.value = data.message;
   }
 }
